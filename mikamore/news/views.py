58be6d72d4bc0025532682dda50d4c491d538b96
Roman Kim <kim.roman-2002@yandex.ru>
2022-11-03 01:36:06 +0300

    Add News templates. Add admin panel. Modify Initial migration.

--- mikamore/news/views.py
+++ mikamore/news/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 def questions(request):
     return HttpResponse('My questions')
 
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects
+    context = { 'news': news }
+    return render(request, 'news/index.html', context)
