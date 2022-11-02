f23341837f05b5175c365a57339b9dc4823449f1
Roman Kim <kim.roman-2002@yandex.ru>
2022-11-03 02:43:46 +0300

    Add News category and template

--- mikamore/news/views.py
+++ mikamore/news/views.py
@@ -6,6 +6,6 @@ def questions(request):
     return HttpResponse('My questions')
 
 def index(request):
-    news = News.objects
+    news = News.objects.all()
     context = { 'news': news }
     return render(request, 'news/index.html', context)
